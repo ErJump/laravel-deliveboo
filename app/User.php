087add7eccc78843cd01965a2ffb41912da6d3c6
markcaf <marcocafassog@gmail.com>
2022-10-11 16:36:35 +0200

    feat: :sparkles: add fillable to User model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,14 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 
+        'email', 
+        'password',
+        'address',
+        'p_iva',
+        'phone_number',
+        'description',
+        'image',
     ];
 
     /**
