86721dc3d5836c7072c6fc3735aa7f48f1f2e10e
ErJump <fedelijunior@gmail.com>
2022-10-12 22:22:09 +0200

    style: :lipstick: Styles show blade

--- resources/views/auth/show.blade.php
+++ resources/views/auth/show.blade.php
@@ -11,19 +11,19 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">{{ $user->restaurant_name }}</h1>
+                <h1 class="text-center">{{ $user->name }}</h1>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-header">
-                        <img src="{{$user->image}}" alt="image">
+                    <div class="card-header ms_ojbectfit_100 p-0">
+                        <img class="ms_objectfit_100" src="{{$user->image}}" alt="image">
                     </div>
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-12 col-md-6">
-                                <h2 class="text-center">Dati personali</h2>
+                            <div class="col-12 col-md-6 mb-3">
+                                <h2 class="text-center mb-3">Dati personali</h2>
                                 <ul class="list-group">
                                     <li class="list-group-item">Email: {{ $user->email }}</li>
                                     <li class="list-group-item">Indirizzo: {{ $user->address }}</li>
@@ -32,8 +32,8 @@
                                 </ul>
                             </div>
                             <div class="col-12 col-md-6">
-                                <h2 class="text-center">Gestione</h2>
-                                <div class="d-flex">
+                                <h2 class="text-center mb-3">Gestione</h2>
+                                <div class="d-flex justify-content-around">
                                     <a class="btn ms_btn_secondary ms-5" href="{{-- {{ route('admin.plates.index') }} --}}">Gestisci i tuoi piatti</a>
                                     <a class="btn btn-primary" href="{{-- {{ route('admin.orders.index') }} --}}">Gestisci i tuoi ordini</a>
                                 </div>
