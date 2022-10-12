b5f435f9270c96278af328a15f59b1f2e33bcca3
ErJump <fedelijunior@gmail.com>
2022-10-12 12:30:32 +0200

    feat: :sparkles: Adds small logo for mobile viewport to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,8 @@
     <nav class="navbar navbar-dark navbar-expand-lg px-5">
         <div class="container-fluid d-flex">
             <a class="navbar-brand" href="#">
-                <img src="{{asset('images/logo-white.png')}}" alt="logo">
+                <img class="d-none d-sm-none d-md-none d-lg-block d-xl-block" src="{{asset('images/logo-white.png')}}" alt="logo">
+                <img class="d-block d-sm-block d-md-block d-lg-none d-xl-none" src="{{asset('images/logo-small.png')}}" alt="logo-small">
             </a>
             <div class="d-flex justify-content-between">
                 <i class="fa-solid fa-cart-shopping text-white fa-2x justify-self-end me-4"></i>
