009745b336ce79568e2f2b38d00e8a8c16b539f8
ErJump <fedelijunior@gmail.com>
2022-10-12 13:54:50 +0200

    fix: :bug: Fixes main div, applies its on single pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Deliveboo | @yield('title')</title>
 
     
 
@@ -21,9 +21,9 @@
 <body>
     <div id="app">
         @include('partials.header')
-        <main class="ms_bg_image_black">
-            @yield('content')
-        </main>
+        
+        @yield('content')
+
         @include('partials.footer')
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
