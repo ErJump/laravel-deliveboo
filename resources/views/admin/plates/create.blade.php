8d40ddd40b25f1b69e77e7ac64f853fdd9c69adb
ErJump <fedelijunior@gmail.com>
2022-10-13 10:28:04 +0200

    feat: :sparkles: Adds plates create blade

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
-@section('title', 'Modifica Piatto')
+@section('title', 'Crea Piatto')
 
 @section('content')
 <main>
     <div class="container-lg">
-        <h2 class="mb-3">Modifica Piatto</h2>
+        <h2 class="mb-3">Crea Piatto</h2>
         <div class="row">
             <div class="col-12">
                 @include('admin.plates.partials.platesForm', [
-                    'route' => 'admin.plates.update',
-                    'method' => 'PUT',
+                    'route' => 'admin.plates.store',
+                    'method' => 'POST',
                 ])
             </div>
         </div>
