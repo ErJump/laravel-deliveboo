e701ce90e08a1fc7bf1685c7896e21a421ef82c5
Antonius131 <antomendetta@gmail.com>
2022-10-13 15:48:19 +0200

    creating index.blade layout

---
+++ resources/views/admin/orders/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+   <main>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <h1 class="text-center">Ordini</h1>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-12">
+               <div class="card">
+                  <table class="table">
+                     <thead>
+                        <tr>
+                           <th scope="col">ID Ordine</th>
+                           <th scope="col">Nome</th>
+                           <th scope="col">Cognome</th>
+                           <th scope="col">Email</th>
+                           <th scope="col">Telefono</th>
+                           <th scope="col">Commento</th>
+                           <th scope="col">Indirizzo</th>
+                           <th scope="col">Totale</th>
+                           <th scope="col">Stato Pagamento</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        <tr>
+                           @foreach ($orders as $order)
+                              <th scope="row">1</th>
+                                 <td>{{ $order->id}}</td>
+                                 <td>{{ $order->first_name}}</td>
+                                 <td>{{ $order->last_name}}</td>
+                                 <td>{{ $order->email}}</td>
+                                 <td>{{ $order->phone}}</td>
+                                 <td>{{ $order->comment}}</td>
+                                 <td>{{ $order->address}}</td>
+                                 <td>{{ $order->total_price}}</td>
+                                 <td>{{ $order->payment_state}}</td>
+                              </tr> 
+                           @endforeach
+                     </tbody>
+                   </table>
+               </div>
+            </div>
+         </div>
+      </div>
+   </main>
+@endsection
