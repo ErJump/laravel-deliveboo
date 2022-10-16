e6796a8e23ef0e9eb086c7d0f76280ee3ae84c00
amaljom <amaljom99@gmail.com>
2022-10-16 18:55:44 +0200

    feat: :sparkles: added blade for custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+<div class="error">
+    <div class="d-flex centering align-items-center">
+        <div>
+            <h1 class="errorquattro">4</h1>
+        </div>
+        <div>
+            <img class="" src="{{asset('assets/images/logo-white.png')}}" alt="logo">
+        </div>
+        <div>
+            <h1 class="errorquattro">4</h1>
+        </div>
+    </div>
+    <div class="text-error d-flex align-items-center flex-column text-white">
+        <h1>"Oops, Page not found. </h1>
+        <h1>Let's go blame the graphic designer"</h1>
+    </div>
+</div>
