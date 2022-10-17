10330054997a7030377c50de1ceee5f923b280bb
amaljom <amaljom99@gmail.com>
2022-10-17 10:23:48 +0200

    style changes and responsive implemented

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -15,9 +15,9 @@
             <h1 class="errorquattro">4</h1>
         </div>
     </div>
-    <div class="text-error d-flex align-items-center flex-column">
-        <h1 class="text-white">"Oops, pagina non trovata. </h1>
-        <h1 class="text-white">Andiamo ad incolpare i graphic designer"</h1>
+    <div class="text-error d-flex align-items-center flex-column text-center ">
+        <h1 class="text-white col-sm-8 font-weight-bold">"Oops, pagina non trovata. </h1>
+        <h1 class="text-white col-sm-10 font-weight-bold">Andiamo ad incolpare i graphic designer"</h1>
     </div>
 </div>
 @endsection
