4b844248d967d06fbb7d020de3917af1f76b747e
Antonius131 <antomendetta@gmail.com>
2022-10-17 11:41:30 +0200

    change html syntax

--- resources/views/admin/plates/partials/platesForm.blade.php
+++ resources/views/admin/plates/partials/platesForm.blade.php
@@ -11,7 +11,7 @@
                     minlength="3" maxlength="255"
                     value="{{old('name', $plate->name)}}">
                     <div>
-                        <small>Min:3/Max:255 caratteri.</small>
+                        <small>Min:3 / Max:255 caratteri.</small>
                     </div>
                 @error('name')
                 <div class="alert alert-danger" role="alert">
@@ -52,6 +52,9 @@
                 <input name="image" type="file" class="form-control" id="image"
                     accept="image/png, image/jpeg, image/jpg"
                     value="{{old('image', $plate->image)}}">
+                    <div>
+                        <small>Max: 2MB</small>
+                    </div>
                 @error('image')
                 <div class="alert alert-danger" role="alert">
                     {{$message}}
@@ -67,7 +70,7 @@
                 <label for="description">Descrizione*</label>
                 <textarea name="description" id="description" rows="5" class="form-control" minlength="5" maxlength="1000">{{old('description', $plate->description)}}</textarea>
                 <div>
-                    <small>Min:5/Max:1000 caratteri.</small>
+                    <small>Min:5 / Max:1000 caratteri.</small>
                 </div>
                 @error('description')
                 <div class="alert alert-danger" role="alert">
@@ -81,7 +84,7 @@
                 <label for="ingredients">Ingredienti*</label>
                 <textarea name="ingredients" id="ingredients" rows="4" class="form-control" minlength="5" maxlength="1000">{{old('ingredients', $plate->ingredients)}}</textarea>
                 <div>
-                    <small>Min:5/Max:1000 caratteri.</small>
+                    <small>Min:5 / Max:1000 caratteri.</small>
                 </div>
                 @error('ingredients')
                 <div class="alert alert-danger" role="alert">
@@ -90,26 +93,23 @@
                 @enderror
             </div>
             {{-- Disponibilità --}}
-            <div class="form-group d-flex px-4 ">
-                <div class="d-flex align-items-center mr-5">
+            <div class="form-group">
+                <div>
+                    <label class="d-block" for="avaible">Disponibilità*</label>
+                </div>
+                <div class="d-flex align-items-center mr-5 px-4">
                     <input class="form-check-input" type="radio" id="available" name="availability" value="1" {{old('availability', $plate->availability) == 1 ? 'checked' : '' }}>
                     <label class="mb-0" for="available">Disponibile</label>
                 </div>
-                <div class="d-flex align-items-center">
+                <div class="d-flex align-items-center px-4">
                     <input class="form-check-input" type="radio" id="notAvailable" name="availability" value="0" {{old('availability', $plate->availability) == 0 ? 'checked' : '' }}>
                     <label class="mb-0"for="notAvailable">Non disponibile</label>
                 </div>
-
-
-
                 @error('availability')
                 <div class="alert alert-danger" role="alert">
                     {{$message}}
                 </div>
                 @enderror
-            </div>
-            <div>
-                <small>Campo Obligatorio.</small>
             </div>
         </div>
         <div class="col-xs-12 d-flex justify-content-end align-items-center w-100 p-3">
