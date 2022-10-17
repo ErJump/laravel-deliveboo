a850972402b9e4d8ad0849afa8f43bf39eb49f4a
ErJump <fedelijunior@gmail.com>
2022-10-17 15:54:04 +0200

    feat: :sparkles: Adds getRestaurant method to HomePage component

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -5,21 +5,35 @@
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
     name: 'HomePage',
     components: {
-
+        
     },
     data() {
         return {
-
+            restaurantsUrl: 'http://localhost:8000/api/users',
+            users: [],
+            typologies: [],
         }
     },
     methods: {
-
+        getRestaurants() {
+            axios.get(this.restaurantsUrl)
+                .then(response => {
+                    this.users = response.data.results.data;
+                    console.log(this.users)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
     },
     created() {
-
+        this.getRestaurants()
     }
 }
 </script>
