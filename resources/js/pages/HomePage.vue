3fb689844d8b41722c08cb0f439b1a82e2fe63ba
ErJump <fedelijunior@gmail.com>
2022-10-17 15:57:48 +0200

    fix: :bug: Changes users with restaurants

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -11,12 +11,12 @@ import axios from 'axios';
 export default {
     name: 'HomePage',
     components: {
-        
+
     },
     data() {
         return {
             restaurantsUrl: 'http://localhost:8000/api/users',
-            users: [],
+            restaurants: [],
             typologies: [],
         }
     },
@@ -24,8 +24,8 @@ export default {
         getRestaurants() {
             axios.get(this.restaurantsUrl)
                 .then(response => {
-                    this.users = response.data.results.data;
-                    console.log(this.users)
+                    this.restaurants = response.data.results.data;
+                    console.log(this.restaurants)
                 })
                 .catch(error => {
                     console.log(error)
