92d4671c7b6d83e30010a98f7523d07386e28d3d
Antonius131 <antomendetta@gmail.com>
2022-10-17 16:20:03 +0200

    creating component structure

---
+++ resources/js/components/MainHeader.vue
@@ -0,0 +1,41 @@
+<template>
+   <header>
+      <nav class="navbar navbar-expand-lg navbar-dark ms_bg_primary py-2">
+         <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+               <img src="../../../public/assets/images/logo-white.png" alt="logo">
+            </a>
+            <div class="d-flex justify-content-between">
+               <i class="fa-solid fa-cart-shopping text-white ustify-self-end"></i>
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+               </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+               <ul class="navbar-nav">
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Ristoranti</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Contattaci</a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </nav>
+   </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
