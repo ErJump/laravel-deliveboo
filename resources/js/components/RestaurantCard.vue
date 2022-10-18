e13cbe821f9d950600909392fffd3e6f324891a4
Antonius131 <antomendetta@gmail.com>
2022-10-18 11:46:15 +0200

    changed card layout

--- resources/js/components/RestaurantCard.vue
+++ resources/js/components/RestaurantCard.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="card px-0 rounded mb-4">
+    <div class="card h-100 rounded">
         <img :src="restaurant.image" class="card-img-top" :alt="restaurant.name">
         <div class="card-body">
             <h5 class="card-title font-weight-bold">{{restaurant.name}}</h5>
             <p class="card-subtitle text-muted mb-3">{{restaurant.description}}</p>
+        </div>
+        <div class="card-footer">
             <p class="card-text text-muted mb-1">
                 {{restaurant.address}}
-            </p>
+            </p>  
             <p class="card-text text-muted">
                 {{restaurant.phone_number}}
             </p>
