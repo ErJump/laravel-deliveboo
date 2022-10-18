be3b87cf8117b52a02d905d79aaab05533603eb1
ErJump <fedelijunior@gmail.com>
2022-10-18 16:18:09 +0200

    feat: :sparkles: Adds router-link to restaurant card

--- resources/js/components/RestaurantCard.vue
+++ resources/js/components/RestaurantCard.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="card h-100 rounded">
-        <img :src="restaurant.image" class="card-img-top" :alt="restaurant.name">
-        <div class="card-body">
-            <h5 class="card-title font-weight-bold">{{restaurant.name}}</h5>
-            <p class="card-subtitle text-muted mb-3">{{restaurant.description}}</p>
-        </div>
-        <div class="card-footer">
-            <p class="card-text text-muted mb-1">
-                {{restaurant.address}}
-            </p>
-            <p class="card-text text-muted">
-                {{restaurant.phone_number}}
-            </p>
-        </div>
+        <router-link :to="'/RestaurantShow/' + restaurant.id">
+            <img :src="restaurant.image" class="card-img-top" :alt="restaurant.name">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">{{restaurant.name}}</h5>
+                <p class="card-subtitle text-muted mb-3">{{restaurant.description}}</p>
+            </div>
+            <div class="card-footer">
+                <p class="card-text text-muted mb-1">
+                    {{restaurant.address}}
+                </p>
+                <p class="card-text text-muted">
+                    {{restaurant.phone_number}}
+                </p>
+            </div>
+        </router-link>
     </div>
 </template>
 
