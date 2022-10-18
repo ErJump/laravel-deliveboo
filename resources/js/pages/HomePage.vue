d039e157c227bc20c4c44c5076e3c0ca648c3086
ErJump <fedelijunior@gmail.com>
2022-10-18 09:05:59 +0200

    feat: :sparkles: Adds getTypologies method to homepage component

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -3,6 +3,11 @@
         <MainJumbotron class="mb-5"/>
         <div class="container-lg">
             <div class="row">
+                <div class="col 6 rounded" v-for="typology in typologiesArray" :key="typology.id">
+                    <h6>{{}}</h6>
+                </div>
+            </div>
+            <div class="row">
                 <RestaurantCard v-for="restaurant in restaurants" :key="restaurant.id" :restaurant="restaurant" />
             </div>
         </div>  
@@ -24,12 +29,14 @@ export default {
     data() {
         return {
             restaurantsUrl: 'http://localhost:8000/api/users',
+            typologiesUrl: 'http://localhost:8000/api/typologies',
             restaurants: [],
             typologiesArray: [],
             typologies: [],
         }
     },
     methods: {
+        //prende la lista di tutti i ristoranti
         getRestaurants() {
             axios.get(this.restaurantsUrl)
                 .then(response => {
@@ -39,10 +46,22 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
-        }
+        },
+        //prende la lista di tutte le tipologie
+        getTypologies() {
+            axios.get(this.typologiesUrl)
+                .then(response => {
+                    this.typologiesArray = response.data.results.data;
+                    console.log(this.typologiesArray)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
     },
     created() {
-        this.getRestaurants()
+        this.getRestaurants(),
+        this.getTypologies()
     }
 }
 </script>
