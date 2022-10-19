e3be53157c14a40d04c7df1c4064a9d67fee8cf4
ErJump <fedelijunior@gmail.com>
2022-10-19 11:04:33 +0200

    fix: :lipstick: General style fix in blade 404 & user show

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -36,8 +36,8 @@
                                 <li class="list-group-item">Partita IVA: {{ $user->p_iva }}</li>
                                 <li class="list-group-item">Numero di telefono: {{ $user->phone_number }}</li>
                             </ul>
-                            <a class="btn ms_btn_secondary mr-2" href="{{ route('admin.plates.index') }}">Gestisci i tuoi piatti</a>
-                            <a class="btn btn-primary" href="{{ route('admin.orders.index') }}">Visualizza i tuoi ordini</a>
+                            <a class="btn ms_btn_secondary mr-2 mb-3" href="{{ route('admin.plates.index') }}">Gestisci i tuoi piatti</a>
+                            <a class="btn btn-primary mb-3" href="{{ route('admin.orders.index') }}">Visualizza i tuoi ordini</a>
                         </div>
                     </div>
                     </div>
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,17 +9,6 @@
             <img class="mb-4 w-100 w-md-75" src="{{asset('assets/images/404.png')}}" alt="404">
         </div>
     </div>
-    {{-- <div class="d-flex centering align-items-center">
-        <div>
-            <h1 class="errorquattro">4</h1>
-        </div>
-        <div>
-            <img class="" src="{{asset('assets/images/logo-small.png')}}" alt="logo">
-        </div>
-        <div>
-            <h1 class="errorquattro">4</h1>
-        </div>
-    </div> --}}
     <div class="text-error d-flex align-items-center flex-column text-center ">
         <h2 class="text-white col-sm-8 font-weight-bold">"Oops, pagina non trovata. </h2>
         <h2 class="text-white col-sm-10 font-weight-bold">Andiamo ad incolpare i graphic designer"</h2>
