077984368b7de4765ce77b8b229a6e9e4844cfe0
ErJump <fedelijunior@gmail.com>
2022-10-20 11:30:57 +0200

    Deletes popup scss adds sweet alert 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,9 +32,10 @@
 
         @include('partials.footer')
 
-        @yield('footer-scripts')
-
+        
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @yield('footer-scripts')
 </body>
 </html>
