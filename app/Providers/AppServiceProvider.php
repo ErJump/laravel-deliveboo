bd103a1a434c843d8fd32d7100596c190fe8b9ef
ErJump <fedelijunior@gmail.com>
2022-10-23 12:03:21 +0200

    feat: :sparkles: Adds braintree + order controller api
    Api gives token for braintree to front

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Braintree;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $environment = 'sandbox';
+        $braintree = new \Braintree\Gateway([
+            'environment' => $environment,
+            'merchantId' => '4r8xcbvmm9m9g8kg',
+            'publicKey' => 'ghbt2sf53zp3y4c8',
+            'privateKey' => '8e1ea6e739da72677178bea8739069e3'
+        ]);
+        config(['braintree' => $braintree]);
     }
 }
