a11d31d804c0a6c7c9510b0b53ae9966e298f9e0
ErJump <fedelijunior@gmail.com>
2022-10-25 12:39:34 +0200

    Adds graph for total money per month

--- resources/views/admin/orders/stats.blade.php
+++ resources/views/admin/orders/stats.blade.php
@@ -11,8 +11,11 @@
                     Statistiche Ordini
                 </h2>
             </div>
-            <div class="col-12">
+            <div class="col-12 mb-5">
                 <canvas id="myChart"></canvas>
+            </div>
+            <div class="col-12">
+                <canvas id="myChart2"></canvas>
             </div>
         </div>
       </div>
@@ -29,8 +32,6 @@
 
     //creo un array vuoto per salvare i dati da passare al grafico
     var ordersData = [];
-    
-    //salvo i mesi degli ordini in un array labels da order date prendendo dalla stringa solo anno e mese senza usare slice
 
     //creo un array di mesi con tutti i mesi dal 2021-01 al 2022-12
     var labels = [];
@@ -59,25 +60,62 @@
     }
 
     let data = {
-    labels: labels,
-    datasets: [{
-      label: 'Ordini per mesi',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: ordersData,
-    }]
-  }; 
+        labels: labels,
+        datasets: [{
+            label: 'Ordini per mesi',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: ordersData,
+        }]
+    };
 
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
+    const config = {
+        type: 'line',
+        data: data,
+        options: {}
+    };
 
-  const myChart = new Chart(
-      document.getElementById('myChart'),
-      config
-  );
-  console.log(labels)
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+
+    //secondo grafico 
+
+    var ordersData2 = [];
+
+    //sommo il total_price di ogni ordine per ogni mese
+    var totalPerMonth = [];
+    for (let i = 0; i < labels.length; i++) {
+        var total = 0;
+        for (let j = 0; j < orders.length; j++) {
+            if (orders[j].order_date.includes(labels[i])) {
+                total += orders[j].total_price;
+            }
+        }
+        totalPerMonth.push(total);
+    }
+
+    let data2 = {
+        labels: labels,
+        datasets: [{
+            label: 'Totale incassi per mese',
+            backgroundColor: 'rgb(50,205,50)',
+            borderColor: 'rgb(50,205,50)',
+            data: totalPerMonth,
+        }]
+    };
+
+    const config2 = {
+        type: 'line',
+        data: data2,
+        options: {}
+    };
+
+    const myChart2 = new Chart(
+        document.getElementById('myChart2'),
+        config2
+    );
+    console.log(totalPerMonth)
 </script>
 @endsection
