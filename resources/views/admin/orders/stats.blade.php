6d26d783be5c92d17515b91b9b554c18b64e0d3c
ErJump <fedelijunior@gmail.com>
2022-10-25 11:35:23 +0200

    Sets stats graph

--- resources/views/admin/orders/stats.blade.php
+++ resources/views/admin/orders/stats.blade.php
@@ -21,29 +21,72 @@
 
 @section('footer-scripts')
 <script>
-    const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-    ];
+    console.log('collegati')
 
-    const data = {
-        labels: labels,
-        datasets: [{
-            label: 'Ordini per mese',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-    };
+    //prendo gli ordini dal database php e li salvo in variabile js
+    var orders = @json($orders);
+    console.log(orders)
 
-    const config = {
-        type: 'line',
-        data: data,
-        options: {}
-    };
+    //creo un array vuoto per salvare i dati da passare al grafico
+    var ordersData = [];
+    
+    //salvo gli la data degli ordini in un array labels
+    var labels = orders.map(function(order){
+        return order.created_at;
+    });
+
+    //contro quanti ordini ci sono per ogni data
+    var counts = {};
+    labels.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+
+    //salvo i dati in un array
+    for (var key in counts) {
+        ordersData.push(counts[key]);
+    }
+
+    let data = {
+    labels: labels,
+    datasets: [{
+      label: 'Ordini per mesi',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: ordersData,
+    }]
+  }; 
+
+  /* const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ]; */
+
+  /* const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  }; */
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+  );
+
+
+    
+  
+
 </script>
 @endsection
